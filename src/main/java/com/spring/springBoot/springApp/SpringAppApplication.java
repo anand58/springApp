f04eebbc527d903298bcdee9d0c36cc039d3f53a
Anand <anandt.tiwari12@gmail.com>
2019-04-18 08:33:57 +0530

    cm

--- src/main/java/com/spring/springBoot/springApp/SpringAppApplication.java
+++ src/main/java/com/spring/springBoot/springApp/SpringAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringAppApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAppApplication.class, args);
 	}
